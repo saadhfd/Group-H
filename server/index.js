546f52ef49a22d33864c7b485ee082bb588cd55e
MickyOulu <t3mami05@students.oamk.fi>
2024-04-23 11:20:19 +0300

    resolved the server issue

--- server/index.js
+++ server/index.js
@@ -22,8 +22,8 @@ const cors = require('cors')
 //const { blogRouter } = require('./routes/blog.js')
 const { userRouter } = require('./routes/user.js')
 
-const port = 3001
-const app = express()
+//const port = 3001
+//const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
