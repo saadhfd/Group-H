3c337047dd1990939f605b475e59cebcefc32bc9
MickyOulu <t3mami05@students.oamk.fi>
2024-04-24 10:34:52 +0300

    js added

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const posts_div = document.querySelector('div#posts')
-
-fetch ('http://localhost:3001')
-.then(response => response.json())
-.then(json => {
-    console.log(json)
-}), (error) => {
-    alert(error)
-}
