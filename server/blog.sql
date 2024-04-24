cb6ec12816cb3a7e06de8f178ed3969beb1a1f35
MickyOulu <t3mami05@students.oamk.fi>
2024-04-24 10:21:26 +0300

    added tables and dependencies

--- server/blog.sql
+++ server/blog.sql
@@ -1,8 +1,35 @@
-create table post (
-    id serial primary key,
-    message text not null
+drop table if exists comment;
+drop table if exists post;
+drop table if exists account;
+
+create table account (
+  id serial primary key,
+  email varchar(100) unique not null,
+  password varchar(255) not null
 );
 
-insert into post (message) values ('My Test Message');
+create table post (
+  id serial primary key,
+  title varchar(100) not null,
+  message text not null,
+  image_name varchar(100),
+  saved timestamp default current_timestamp,
+  account_id int not null,
+    constraint fk_account
+      foreign key(account_id)
+        references account(id)
+);
 
-insert into post (message) values ('My Another Test Message');
+create table comment (
+  id serial primary key,
+  comment_text text not null,
+  saved timestamp default current_timestamp,
+  post_id int not null,
+    constraint fk_post
+      foreign key (post_id)
+        references post(id),
+  account_id int not null,
+    constraint fk_account
+      foreign key (account_id)
+        references account(id)
+)
