f86b21cbfc48a264b0ce44138b8eb2fecd0efcd0
MickyOulu <t3mami05@students.oamk.fi>
2024-04-24 10:39:11 +0300

    changes

---
+++ js/class/User.js
@@ -0,0 +1,77 @@
+import { BACKEND_URL } from "../config.js"
+
+class User {
+  #id = undefined
+  #email = undefined
+  #token = undefined
+
+  constructor() {
+    const userFromStorage = sessionStorage.getItem('user')
+    if (userFromStorage) {
+      const userObject = JSON.parse(userFromStorage)
+      this.#id = userObject.id
+      this.#email = userObject.email
+      this.#token = userObject.token
+    }
+  }
+
+  get id() {
+    return this.#id
+  }
+
+  get email() {
+    return this.#email
+  }
+
+  get token() {
+    return this.#token
+  }
+
+  get isLoggedIn() {
+    return this.#id !== undefined ? true : false
+  }
+
+  async login(email,password) {
+    const data = JSON.stringify({email: email,password: password})
+    const response = await fetch(BACKEND_URL + '/user/login',{
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: data
+    })
+    if (response.ok === true) {
+      const json = await response.json()
+      this.#id = json.id
+      this.#email = json.email
+      this.#token = json.token
+      sessionStorage.setItem('user',JSON.stringify(json))
+      return this
+    } else {
+      throw response.statusText
+    }
+  }
+
+  async register(email,password) {
+    const data = JSON.stringify({email: email,password: password})
+    const response = await fetch(BACKEND_URL + '/user/register',{
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: data
+    })
+    if (response.ok === true) {
+      const json = await response.json()
+      return json.id
+    } else {
+      throw response.statusText
+    }
+  }
+
+  logout() {
+    this.#id = undefined
+    this.#email = undefined
+    this.#token = undefined
+    sessionStorage.removeItem('user')
+  }
+
+}
+
+export { User }
